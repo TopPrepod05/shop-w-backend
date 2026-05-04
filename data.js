ad2c78b12a8b1ed67c91646e3690824489c64d46
Tim <lordenure@gmail.com>
2026-05-04 18:34:53 +0300

    ПЕРВЫЙ КОММИТ

---
+++ data.js
@@ -0,0 +1,35 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Компьютер ASUS Intel Core I3',
+        price: 25700,
+        image: `./images/PC.png`
+    },
+    {
+        id: 2,
+        name: 'Кроссовки спортивные на платформе',
+        price: 1593,
+        image: `./images/boots.png`
+    },
+    {
+        id: 3,
+        name: 'Подушка 70х70 - 2 шт',
+        price: 973,
+        image: `./images/pillow.png`
+    },
+    {
+        id: 4,
+        name: 'Смартфон Galaxy A17 (8GB+256GB) Black, Черный',
+        price: 16791,
+        image: `./images/Galaxy A17.png`
+    },
+    {
+        id: 5,
+        name: 'Спортивные штаны',
+        price: 1394,
+        image: `./images/Jeans.png`
+    },
+]
+
+
+export const cart = []
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        shaman: "МОЙ ССАЙТ!",
+        cssname: "products"
+    })
+})
+
+app.get('/products', (req, res) => {
+    res.render('products', {
+        shaman: 'ПРОДУКТЫ!',
+        cssname: 'products'
+    })
+})
+
+app.get('/cart', (req, res) => {
+    res.render('cart', {
+        shaman: 'ПРОДУКТЫ!',
+        cssname: 'cart'
+    })
+})
+
+app.listen(5000)
